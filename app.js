3da8b85ee771092953d603378729fbe7dab41626
tankTopTaro <balmoresk3v1n@gmail.com>
2024-08-30 20:46:46 +0800

    node+ejs

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.listen(3000)
+
+app.get('/', (req, res) => {
+    //res.send('<p>Home Page</p>')
+    //res.render('./views/index', { root: __dirname })
+    const blogs = [
+        {title: 'Blog A', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Blog B', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Blog C', snippet: 'Lorem ipsum dolor sit amet consectetur'}
+    ]
+    res.render('index', { title: 'Home', blogs })
+})
+
+
+app.get('/about', (req, res) => {
+    //res.send('<p>About Page</p>')
+    //res.sendFile('./views/about.ejs', { root: __dirname })
+    res.render('about', { title: 'About'})
+})
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create'})
+})
+
+app.use((req, res) => {
+    //res.status(404).sendFile('./views/404.ejs', { root: __dirname })
+    res.status(404).render('404', { title: '404'})
+})
