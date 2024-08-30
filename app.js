38372eace0aad732b63fadad62e02d3016fa77ce
tankTopTaro <balmoresk3v1n@gmail.com>
2024-08-31 05:43:34 +0800

    Add .gitignore with node_modules

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 
 app.listen(3000)
+
+// middleware & static files
+app.use(express.static('public'))
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     //res.send('<p>Home Page</p>')
