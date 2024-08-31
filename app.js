31f00f48c06f99f67f61998534e82fbaa2c41728
tankTopTaro <balmoresk3v1n@gmail.com>
2024-09-01 06:44:38 +0800

    implemented POST and DELETE methods for Blogs

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.set('view engine', 'ejs')
 // middleware & static files
 app.use(express.static('public'))
 
+app.use(express.urlencoded({ extended: true }))
+
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
@@ -39,6 +41,39 @@ app.get('/blogs', (req, res) => {
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create'})
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs')
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+})
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id
+    Blog.findById(id)
+        .then((result) => {
+            res.render('details', {blog: result, title: 'Blog Details'})
+        })
+        .catch(err => {
+            console.error(err)
+        })
+})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            res.json({ redirect: '/blogs' })
+        })
+        .catch(err => {
+            console.error(err)
+        })
 })
 
 app.use((req, res) => {
