f106717660ebf8084815f59609400c585b5312a8
tankTopTaro <balmoresk3v1n@gmail.com>
2024-09-01 08:04:36 +0800

    Implemented MVC

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogRoutes = require('./routes/blogRoutes')
 
 const app = express()
 
@@ -28,53 +28,7 @@ app.get('/about', (req, res) => {
     res.render('about', { title: 'About'})
 })
 
-app.get('/blogs', (req, res) => {
-    Blog.find()
-        .sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', {title: 'All Blogs', blogs: result})
-        })
-        .catch((err) => {
-            console.error(err)
-        })
-})
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create'})
-})
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body)
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs')
-        })
-        .catch((err) => {
-            console.error(err)
-        })
-})
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id
-    Blog.findById(id)
-        .then((result) => {
-            res.render('details', {blog: result, title: 'Blog Details'})
-        })
-        .catch(err => {
-            console.error(err)
-        })
-})
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            res.json({ redirect: '/blogs' })
-        })
-        .catch(err => {
-            console.error(err)
-        })
-})
+app.use('/blogs', blogRoutes)
 
 app.use((req, res) => {
     //res.status(404).sendFile('./views/404.ejs', { root: __dirname })
